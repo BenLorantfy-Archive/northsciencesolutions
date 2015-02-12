46ebcffc1a6bf0298bf6e94d39422be4dd283dd8
takumi111 <takumi114@gmail.com>
2015-02-12 15:57:36 -0500

    Navbar button

--- index.php
+++ index.php
@@ -3,7 +3,92 @@
 <head>
 	<?php include("includes/head.php"); ?>
 </head>
-<body>
-	hi
-</body>
+<header>
+	<nav class="navbar navbar-inverse navbar-fixed-top">
+	  <div class="container-fluid">
+		<div class="navbar-header">
+		  <a class="navbar-brand" href="#">WebSiteName</a>
+		<button class="navbar-toggle" data-toggle="collapse" data-target=".navHeaderCollapse">
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+		</button>
+		</div>
+
+		<div class="navbar-collapse collapse navHeaderCollapse">
+		  <ul class="nav navbar-nav">
+			<li class="active"><a href="#">Home</a></li>
+			<li><a href="#">Page 1</a></li>
+			<li><a href="#">Page 2</a></li>
+			<li><a href="#">Page 3</a></li>
+		  </ul>
+		</div>
+	  </div>
+	</nav>
+<div class="jumbotron"></div>
+</header>
+<section>
+	<body>
+		<div class="container">
+			<div class="team">
+				<div class="row">
+					<div class="col-md-12">
+						<p>Team</p>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-4">
+						<img src="img/homer.png" />
+						<p>Jack Johnson</p>
+						<p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostru</p>
+					</div>
+					<div class="col-md-4">
+						<img src="img/homer.png" />
+						<p>Jack Johnson</p>
+						<p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostru</p>
+					</div>
+					<div class="col-md-4">
+						<img src="img/homer.png" />
+						<p>Jack Johnson</p>
+						<p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostru</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="container">
+			<div class="team">
+				<div class="row">
+					<div class="col-md-6">
+						<img src="img/homer.png" />
+					</div>
+					<div class="col-md-6">
+						<p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostru</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="container">
+			<div class="team">
+				<div class="row">
+					<div class="col-md-6">
+						<div class="test">a</div>
+					</div>
+					<div class="col-md-6">
+						<div class="test">a</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-6">
+						<div class="test">a</div>
+					</div>
+					<div class="col-md-6">
+						<div class="test">a</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+</section>
+<footer>
+</footer>
 </html>
