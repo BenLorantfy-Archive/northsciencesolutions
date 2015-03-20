4a8a1c32e39e832201b32c126f13671bf7a4f065
takumi111 <takumi114@gmail.com>
2015-03-20 16:07:23 -0400

    revision
    Last revision we made on Wednesday did not go through for some reason

--- includes/head.php
+++ includes/head.php
@@ -6,6 +6,8 @@
 
 <!-- CSS -->
 <link rel="stylesheet" type="text/css" href="css/styles.css"/>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+<link rel="stylesheet" href="css/custom.css">
 
 <!-- JS -->
 <script src = "js/main.js"></script>
@@ -14,3 +16,7 @@
 <script src = "js/frameworks/jqease.js"></script> 	<!-- Easing for jquery's animate function -->
 <script src = "js/frameworks/msgbox.js"></script> 	<!-- Used to show quick messages to the user (i.e. saved succesfully ) -->
 <script src = "js/frameworks/postcall.js"></script> <!-- Used to call php functions from js more easily -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script> <!-- BootStrap -->
+<!-- Needed to add these in order for navbar to work properly in mobile mode -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<script src="js/frameworks/bootstrap.min.js"></script>
